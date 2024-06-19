2d689d9112d9554c6feefc461c3e9b502989cf32
Moealsir <moealsirnikestore@gmail.com>
2024-06-20 00:35:11 +0300

    first commit

---
+++ app/sections/Projects.jsx
@@ -0,0 +1,10 @@
+
+const Projects = () => {
+  return (
+    <section id="projects" className='padding-x max-container bg-blue-20 w-full min-h-screen justify-center items-center flex'>
+        <div>Projects</div>
+    </section>
+  )
+}
+
+export default Projects
