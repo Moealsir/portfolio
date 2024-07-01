b29b7b4a6231690681a2d2b0b32c21310044d13e
Moealsir <mohamedwdalsir@gmail.com>
2024-07-01 08:20:35 +0300

    Update index.js

--- app/assets/images/index.js
+++ app/assets/images/index.js
@@ -1,20 +1,38 @@
 
 
 import portfolio from './projects/portfolio.png'
-import alx from './projects/alx.png'
-import imdb from './projects/imdb.png'
-import twitterout from './projects/twitterout.png'
+
+import alxproj from './projects/alxreminder/alx-projects.png'
+import alxterm from './projects/alxreminder/alx-terminal.png'
+import alxmsg from './projects/alxreminder/alx-message.jpeg'
+
 import bettyfixer from './projects/bettyfixer.png'
 import weatherwave7 from './projects/weatherwave7.png'
 
+import tcexcelout from './projects/twitterchecker/exceloutput.png'
+import tcjsonout from './projects/twitterchecker/jsonoutput.png'
+
+import imdb from './projects/imdb/imdb.png'
+import ondemand from './projects/imdb/ondemand.png'
+import exceloutput from './projects/imdb/exceloutput.png'
+import sqloutput from './projects/imdb/sqloutput.png'
 export {
 
 
     portfolio,
-    imdb,
-    alx,
-    twitterout,
+
+    alxproj,
+    alxterm,
+    alxmsg,
+
     bettyfixer,
     weatherwave7,
-
+    
+    tcexcelout,
+    tcjsonout,
+    
+    imdb,
+    ondemand,
+    exceloutput,
+    sqloutput,
 }
